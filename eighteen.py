aa6291245c6496bdd37b6ba24c4fd5bfb83a671c
Basketball20099 <157422433+Basketball20099@users.noreply.github.com>
2024-09-13 13:27:50 -0700

    Create eighteen.py

---
+++ eighteen.py
@@ -0,0 +1,50 @@
+#   a114_nested_loops_4.py 
+import turtle as trtl
+
+painter = trtl.Turtle()
+painter.penup()
+painter.goto(-200, 0)
+painter.pendown()
+
+x = -200
+y = 0
+move_x = 25  
+move_y = 25
+while (x < 200):
+
+  while (y < 100):
+    x = x + move_x
+    y = y + move_y
+    painter.goto(x,y)
+  move_y = -25
+  
+  while (y > 0):
+    x = x + move_x
+    y = y + move_y
+    painter.goto(x,y)
+  move_y = 25
+#Go back to starting point and reset values.
+painter.penup()
+painter.goto(-200, 0)
+painter.pendown()
+x = -200
+y = 0
+move_x = 25  
+move_y = 25
+while (x < 200):
+#run similar loop again
+  while (y > -100):
+    x = x + move_x
+    y = y - move_y
+    painter.goto(x,y)
+  
+  while (y < 0):
+    x = x + move_x
+    y = y + move_y
+    painter.goto(x,y)
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
+
